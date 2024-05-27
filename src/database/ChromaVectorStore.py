5b0388f4599a13da37ee4d659cefdf5331f84981
JR Blanco <jr.cosio1@gmail.com>
2024-05-27 18:58:57 +0200

    Cambiada clase BDChroma por ChromaVectorStorage

--- src/database/ChromaVectorStore.py
+++ src/database/ChromaVectorStore.py
@@ -1,48 +1,107 @@
-import sys
-import os
-
-# Añade el directorio 'src' al PYTHONPATH
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from BDChroma import BDChroma
-from scrapper.LoadPDF import LoadPDF
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
+import ollama, chromadb
 
 class ChromaVectorStore:
-    def __init__(self, host="localhost", port=8000, carpeta_pdf='../data', bloque_datos=2):
+    def __init__(self, host="localhost", port=8000, collection_name="default"):
         self.host = host
         self.port = port
-        self.carpeta_pdf = carpeta_pdf
-        self.bloque_datos = bloque_datos
-        
+        self.collection_name = collection_name
+
         self.vectorstore = None
+        self.embeddings = OllamaEmbeddings(model="llama3")
+        self.text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1024, chunk_overlap=0, length_function=len)
+
         
-        self.loadPDF = LoadPDF(carpeta_pdf=self.carpeta_pdf, bloque_datos=self.bloque_datos)
         self._initialize_vectorstore()
 
     def _initialize_vectorstore(self):
         """
         Inicializa la base de datos Chroma.
+
+        Este método se utiliza para inicializar la base de datos Chroma. 
+        Establece una conexión con el servidor de Chroma utilizando el host y el puerto especificados.
+        Si la conexión es exitosa, se crea una instancia de Chroma y se asigna a la variable vectorstore.
+        La función de embedding especificada también se asigna a la instancia de Chroma.
+
+        Raises:
+            ValueError: Si no se puede conectar al servidor de Chroma.
+
         """
         try:
-            self.vectorstore = BDChroma(collection_name="chatBOC-chroma", host=self.host, port=self.port)
-        except:
-            print("Error: El servidor de Chroma no está disponible.")
+            self.client = chromadb.HttpClient(host=self.host, port=self.port)
+            print("Conexión exitosa con el servidor de Chroma") #Logging
+            self.vectorstore = Chroma(client=self.client, embedding_function=self.embeddings)
+            print("Base de datos Chroma inicializada.") #Logging
+        except ValueError:
+            print("Error: No se pudo conectar al servidor de Chroma.") #Looging
+            print(ValueError) #Logging
+                
+            
 
-    def add_documents(self):
+    def add_documento(self, documento):
         """
-        Agrega los documentos PDF a la base de datos Chroma.
+        Añade un documento a la base de datos Chroma, dividiendo el documento en chunks.
+
+        Parámetros:
+        - documento: El documento que se va a añadir a la base de datos Chroma.
+
         """
-        pdf_texts = self.loadPDF.load_bloque()
-       
-        #self.vectorstore.add_documents(pdf_texts)
+        try:
+            docs_chuncks = self.text_splitter.split_documents(documento)
+            print("Documento dividido en chunks.") #Logging
+            self.vectorstore = Chroma.from_documents(documents=docs_chuncks, embedding=self.embeddings, client=self.client)
+            print("Documento añadido a la base de datos Chroma.") #Logging
+        except ValueError:
+            print("Error al añadir el documento a la base de datos Chroma.") #Logging
+            print(ValueError) #Logging
+    
+    def add_list_documentos(self, documentos):
+        """
+        Añade una lista de documentos a la base de datos Chroma.
+        """
+        for documento in documentos:
+            self.add_documento(documento)
+    
+     
+    def _combine_docs(self, docs):
+        """
+        Combina los documentos en una sola cadena de texto.
+
+        Args:
+            docs (list): Una lista de documentos.
+
+        Returns:
+            str: La cadena de texto que contiene la combinación de los documentos.
+        """
+        combined_docs = ""
+        for doc in docs:
+            combined_docs += doc.page_content + "\n"
+        return combined_docs.rstrip('\n')
+    
+    def get_documents(self, query):
+        """
+        Devuelve los documentos que coinciden con la query en un solo string, que sera el que pasemos al modelo LLM.
+        Es decir esta función es la que le da el constexto al LLM.
+        """        
+        retrieved_docs = self.vectorstore.as_retriever().invoke(query)
+        return self._combine_docs(retrieved_docs)
+    
         
-        for pdf_text in enumerate(pdf_texts):
-            self.vectorstore.add_documents(pdf_text)
-            
-            
-        
-        
+
+                
 #ejemplo de uso
-chromavector = ChromaVectorStore()
+# chromavector = ChromaVectorStore()
 
-chromavector.add_documents()
+
+# loader = PyPDFLoader('./src/database/boc_7.pdf')
+# paginas = loader.load()
+
+# chromavector.add_documento(paginas)
+
+# text = chromavector.get_documents("Información pública del acuerdo provisional de modifica-ción de la Tasa por prestación del Servicio de Recogidade Basura")
+
+# print(text)
+

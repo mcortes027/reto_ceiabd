07b9dc1344cc6577952c7a880ed8439c0f62a9e2
JuanCarlosGonzalezFernandez <juancgonzalezfernandez@gmail.com>
2024-05-31 15:20:15 +0200

    Modificar archivo 'Chatbot.py'.

--- src/web_app/pages/Chatbot.py
+++ src/web_app/pages/Chatbot.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os, sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from rag.Rag import Rag
 

875e71210424daf63c335a361f6c5fb61d7952ef
mcortes027 <manolocorte.educacion@protonmail.com>
2024-06-04 18:48:36 +0200

    pepe

---
+++ despliegues/config.py
@@ -0,0 +1,10 @@
+# Este archivo contempla los distintos entornos en los que permitimos despliegues, para añadir un nuevlo entorno copie la templeta.
+# Y edítela según necesite. 
+
+# ENTORNO PARA DESPLIEGUE EN CLASE. 
+clase ={
+      "volumen_chromadb":"/SSD1/home/ciabd12/volumenes_produccion/chromadb-data",
+      "volumen_ollama":"/reto_ceiabda_produccion/ollama-data",
+      "volumen_mysql":"/reto_ceiabda_produccion/mysql-data"
+} 
+

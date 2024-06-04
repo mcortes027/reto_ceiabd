370cf5edc17e731bdbd56e2f7029b25b0bd4a28b
mcortes027 <manolocorte.educacion@protonmail.com>
2024-06-04 19:19:32 +0200

    aaa

--- despliegues/levantar.py
+++ despliegues/levantar.py
@@ -6,10 +6,14 @@ import sys
 
 try:
     entorno = sys.argv[1:] 
+    print(f"Se ha detectado el entorno:{entorno}")
     variables_entorno = cfg.entornos[entorno]
     os.environ['VOLUMEN_CHROMADB'] = variables_entorno['volumen_chromadb']
+    print(f"Se ha asignado el path VOLUMEN_CHROMADB:{variables_entorno['volumen_chromadb']}")
     os.environ['VOLUMEN_OLLAMA'] = variables_entorno['volumen_ollama']
+    print(f"Se ha asignado el path VOLUMEN_OLLAMA:{variables_entorno['volumen_ollama']}")
     os.environ['VOLUMEN_MYSQL'] = variables_entorno['volumen_mysql']
+    print(f"Se ha asignado el path VOLUMEN_MYSQL:{variables_entorno['volumen_mysql']}")
 except:
     print("Por favor recuerda pasar como argumento el nombre del entorno en el que estás desplegando")
     print("El entorno ha de estar definido en config.py")
